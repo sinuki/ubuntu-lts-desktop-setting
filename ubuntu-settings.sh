8fcc29caf8cf523ac12878f902936ffc29c0118a
sinuki <parksw7302@gmail.com>
2018-06-15 23:18:58 +0900

    chore. maintain installation option

--- ubuntu-settings.sh
+++ ubuntu-settings.sh
@@ -60,9 +60,9 @@ sudo apt install -y filezilla
 if [ ! -f `which subl` ]
 then
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt install apt-transport-https
+	sudo apt install -y apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt install sublime-text
+	sudo apt install -y sublime-text
 fi
 
 # vscode
