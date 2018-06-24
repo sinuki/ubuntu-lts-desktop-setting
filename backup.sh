d7cf7e37b067732dfe30dc4ed033838abe61c175
sinuki <parksw7302@gmail.com>
2018-06-24 18:17:22 +0900

    feat. display process time

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@
 # author : parksw
 # since : 2018.06.16
 
+START=`date +%s`
 BACKUP_FILE_NAME="ubuntu_$(date +%Y-%m-%d).tar.gz"
 
 sudo tar cvpzf "$BACKUP_FILE_NAME" \
@@ -18,3 +19,11 @@ sudo tar cvpzf "$BACKUP_FILE_NAME" \
 --exclude=*/*Trash* \
 --exclude="*/$BACKUP_FILE_NAME" \
 /
+
+END=`date +%s`
+PROCESS_TIME=`expr $END - $START`
+PROCESS_HOURS=`expr $PROCESS_TIME / 60 / 24`
+PROCESS_MINUTES=`expr $PROCESS_TIME / 60`
+PROCESS_SECONDS=`expr $PROCESS_TIME % 60`
+
+echo "[Complete] process time : $PROCESS_HOURS hours $PROCESS_MINUTES min $PROCESS_SECONDS sec"
